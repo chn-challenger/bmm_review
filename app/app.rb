78fcc3406a2717bd07703695ae04d0f195194257
chn.challenger <chn.challenger@gmail.com>
2015-09-23 20:38:43 +0100

    added password validation

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'models/link.rb'
 require_relative '../data_mapper_setup'
 
@@ -6,6 +7,7 @@ class BookmarkManager < Sinatra::Base
   set :views, proc{File.join(root, '..' , 'views')}
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   helpers do
     def current_user
@@ -48,10 +50,17 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
-                password: params[:password])
-    session[:user_id] = user.id
-    redirect to('/links')
+    @user = User.new(email: params[:email],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
+
   end
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,20 +8,44 @@ feature 'User sign up' do
   # However, we are currently driving everything through
   # feature tests and we want to keep this example simple.
 
+  #
+  # scenario 'I can sign up as a new user' do
+  #   expect { sign_up }.to change(User, :count).by(1)
+  #   expect(page).to have_content('Welcome, alice@example.com')
+  #   expect(User.first.email).to eq('alice@example.com')
+  # end
+  #
+  # def sign_up(email: 'alice@example.com',
+  #             password: 'oranges!')
+  #   visit '/users/new'
+  #   expect(page.status_code).to eq(200)
+  #   fill_in :email,    with: email
+  #   fill_in :password, with: password
+  #   click_button 'Sign up'
+  # end
 
-  scenario 'I can sign up as a new user' do
-    expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, alice@example.com')
-    expect(User.first.email).to eq('alice@example.com')
+  scenario 'requires a matching confirmation password' do
+    # again it's questionable whether we should be testing the model at this
+    # level.  We are mixing integration tests with feature tests.
+    # However, it's convenient for our purposes.
+    expect { sign_up(password_confirmation: 'fak') }.not_to change(User, :count)
+  end
+
+  scenario 'with a password that does not match' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 
   def sign_up(email: 'alice@example.com',
-              password: 'oranges!')
+              password: '12345678',
+              password_confirmation: '12345678')
     visit '/users/new'
-    expect(page.status_code).to eq(200)
-    fill_in :email,    with: email
+    fill_in :email, with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
+
 
 end
