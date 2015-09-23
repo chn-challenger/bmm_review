78183ae652c721ee494b56605c7460733dd7f662
chn.challenger <chn.challenger@gmail.com>
2015-09-23 21:06:48 +0100

    Added empty email validation

--- app/app.rb
+++ app/app.rb
@@ -51,17 +51,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    @user = User.new(email: params[:email],
-                password: params[:password],
-                password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to('/links')
-    else
-      flash.now[:notice] = "Password and confirmation password do not match"
+    if params[:email] == '' || params[:email].nil?
+      flash.now[:notice] = "Must enter an email address"
       erb :'users/new'
+    else
+      @user = User.new(email: params[:email],
+                  password: params[:password],
+                  password_confirmation: params[:password_confirmation])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect to('/links')
+      else
+        flash.now[:notice] = "Password and confirmation password do not match"
+        erb :'users/new'
+      end
     end
-
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, required: true
   # this will store both the password and the salt
   # It's Text and not String because String holds
   # 50 characters by default
